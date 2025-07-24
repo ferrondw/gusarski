7eb6be1c09dda4b35f785f792643858720126146
ferrondw <145978195+ferrondw@users.noreply.github.com>
2025-07-25 00:11:05 +0200

    auth failed display fix

--- public/assets/scripts/auth.js
+++ public/assets/scripts/auth.js
@@ -1,15 +1,12 @@
-if (new URLSearchParams(window.location.search).get('fail') === '1') {
+const params = new URLSearchParams(window.location.search);
+
+if (params.get('fail') === '1') { // idk how else to do it, so when the param is added, show the message and remove it instantly
     document.getElementById('error').style.display = 'block';
     document.getElementById('username').value = '';
     document.getElementById('password').value = '';
-}
 
-window.addEventListener('DOMContentLoaded', () => {
-    const u = document.getElementById('username');
-    const p = document.getElementById('password');
-    setTimeout(() => {
-        if (u.value && p.value) {
-            document.getElementById('loginForm').submit();
-        }
-    }, 200);
-});
+    params.delete('fail');
+    let newQuery = params.toString();
+    let newUrl = window.location.pathname + (newQuery ? '?' + newQuery : '');
+    history.replaceState(null, '', newUrl);
+}
