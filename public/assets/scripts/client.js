c1e9ca688ba4140e7d7c5e5734f983afd9b70eb1
ferrondw <145978195+ferrondw@users.noreply.github.com>
2025-08-18 17:09:09 +0200

    new logo (shield)

--- public/assets/scripts/client.js
+++ public/assets/scripts/client.js
@@ -199,7 +199,7 @@ function renderQueue(queue) {
         <h4 class="queueTitle">${task.data.title}</h4>
         <p class="queueStatus">Status: ${task.state}</p>
         <p class="queueUpdate">${lastMessage}</p>
-        <img class="providerIcon" src="/icons/${providers[task.providerID].icon}">
+        <img class="providerIcon" src="${providers[task.providerID].icon}">
         <div class="queueButtons">
         ${button}
         </div>`;
